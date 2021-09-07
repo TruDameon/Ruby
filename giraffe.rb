0161111a460632250ada49d4c3e8157c74d294b7
Dameon <shubhamsingh0602@gmail.com>
2021-09-07 06:57:32 +0530

    Day1

--- giraffe.rb
+++ giraffe.rb
@@ -1,1 +1,18 @@
-print "Hello World"
+# puts 'Dameon' # Gives a new line to the next sentence
+# print "Hello World \n" # Doesn't print on a new line
+# puts 'Dameon' # Gives a new line to the next sentence
+# puts 'Dameon' # Gives a new line to the next sentence
+
+# character_name = "John"
+# character_age = "35"
+
+# puts ("There once was a man named " + character_name)
+# puts ("he was " + character_age + " years old.")
+# puts ("He really liked the name " + character_name)
+# puts ("but he like being " + character_age + ".")
+
+# name = "Dameon" # String
+# age = 75 # Integer
+# cgpa = 7.43 # Float
+# isMale = true # Boolean
+# flaws = nil # No value
