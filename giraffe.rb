4d2c50a1082d5ecfe810cd87595a032632d047ca
Dameon <shubhamsingh0602@gmail.com>
2021-09-12 05:26:37 +0530

    Day2

--- giraffe.rb
+++ giraffe.rb
@@ -16,3 +16,51 @@
 # cgpa = 7.43 # Float
 # isMale = true # Boolean
 # flaws = nil # No value
+
+# # Working with strings
+# puts "Giraffe \" Academy"
+# puts "Giraffe\nAcademy"
+# phrase = "Giraffe Academy"
+# puts phrase
+# puts phrase.upcase()
+# puts phrase.downcase()
+# puts phrase.strip()
+# puts phrase.length()
+# puts phrase.include? "acad" # Case Sensitive
+# puts phrase[0]
+# puts phrase.index("A")
+# puts phrase.index("ffe")
+
+# # Maths & Numbers
+# puts 5
+# puts 5.46
+# puts -5.62
+# puts 5 + 9
+# puts 5 - 9
+# puts 5 * 9
+# puts 5 / 9
+# puts 2**3
+# puts 10 % 3
+# num = 20
+# puts num
+# puts ("my fav num " + num.to_s)
+# puts num.abs()
+# puts num.round()
+# puts num.ceil()
+# puts num.floor()
+# puts Math.sqrt(36)
+# puts Math.log(10)
+# puts 1 + 7
+# puts 1.0 + 7
+# puts 10/7
+# puts 10/7.0
+
+# User Input
+# puts "Enter your name: "
+# name = gets
+# puts ("Hello " + name)
+# puts ("Hello " + name + ", you are cool!")
+name = gets.chomp()
+puts ("Hello " + name + ", you are cool!")
+age = gets.chomp()
+puts ("Hello " + name + ", you are " + age)
